e0325d6a2d6b0fd0564f2344c9700040fb7963ab
Charlie Chrisman <charlie.chrisman@gmail.com>
2020-04-30 21:39:47 +0200

    fix typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: {
-        spaceId: accessToken: process.env.CONTENTFUL_SPACE_ID,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
         host: process.env.CONTENTFUL_HOST
       }
