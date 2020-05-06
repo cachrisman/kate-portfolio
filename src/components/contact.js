b593cd2aa9332a2fb7434bcc98fbd028be33a91c
Charlie Chrisman <charlie.chrisman@gmail.com>
2020-05-06 20:18:43 +0200

    updated contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,7 +1,50 @@
 import React, { Component } from "react";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import axios from "axios"
+import * as qs from "query-string"
 
 export default class Contact extends Component {
+  constructor(props) {
+    super(props)
+    this.domRef = React.createRef()
+    this.state = { feedbackMsg: null }
+  }
+
+  handleSubmit(event) {
+    // Do not submit form via HTTP, since we're doing that via XHR request.
+    event.preventDefault()
+    // Loop through this component's refs (the fields) and add them to the
+    // formData object. What we're left with is an object of key-value pairs
+    // that represent the form data we want to send to Netlify.
+    const formData = {}
+    Object.keys(this.refs).map(key => (formData[key] = this.refs[key].value))
+
+    // Set options for axios. The URL we're submitting to
+    // (this.props.location.pathname) is the current page.
+    const axiosOptions = {
+      url: this.props.location.pathname,
+      method: "post",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      data: qs.stringify(formData),
+    }
+
+    // Submit to Netlify. Upon success, set the feedback message and clear all
+    // the fields within the form. Upon failure, keep the fields as they are,
+    // but set the feedback message to show the error state.
+    axios(axiosOptions)
+      .then(response => {
+        this.setState({
+          feedbackMsg: "Thank you! I'll review your message and get back to you soon!",
+        })
+        this.domRef.current.reset()
+      })
+      .catch(err =>
+        this.setState({
+          feedbackMsg: "Form could not be submitted. Refresh the page to try again.",
+        })
+      )
+  }
+
   render() {
     const { data } = this.props;
     return (
@@ -16,25 +59,22 @@ export default class Contact extends Component {
             </div>
             <div className="left col-md-6 col-lg-6">
               <div className="contact-form">
-                <form
-                  action={`https://formspree.io/${this.props.data}`}
-                  name="contact"
-                  method="POST"
-                  data-netlify="true"
-                >
+              {this.state.feedbackMsg && <p>{this.state.feedbackMsg}</p>}
+                <form ref={this.domRef} name="Contact Form" method="POST" data-netlify="true" onSubmit={event => this.handleSubmit(event)}>
+                  <input ref="form-name" type="hidden" name="form-name" value="Contact Form" />
                   <div>
                     <label>
-                      Your Name: <input type="text" name="name" required />
+                      Your Name: <input ref="name" type="text" name="name" required />
                     </label>
                   </div>
                   <div>
                     <label>
-                      Your Email: <input type="email" name="email" required />
+                      Your Email: <input ref="email" type="email" name="email" required />
                     </label>
                   </div>
                   <div>
                     <label>
-                      Message: <textarea name="message" required></textarea>
+                      Message: <textarea ref="message" name="message" required></textarea>
                     </label>
                   </div>
                   <div>
